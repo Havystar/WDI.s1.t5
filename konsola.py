653744856a4dbbbcfeff57ae66127ab06ccf5739
weronikachr <weronika.chruszcz.2000@gmail.com>
2020-10-26 23:06:25 +0100

    upgradedConverterLiveCurrency

--- konsola.py
+++ konsola.py
@@ -1,14 +1,16 @@
 #1$ = 3,8772zł
+from forex_python.converter import CurrencyRates
+c = CurrencyRates()
 
 print("Którą podajesz walutę? Jeśli zł napisz zl jesli dolar napisz $")
 waluta = input()
 if waluta == "zl":
     print("ile złoówek chcesz zamienić na dolary?")
     zloty = float(input())
-    dolar = float(zloty/3.8772)
+    dolar = c.convert('PLN', 'USD', zloty)
     print("za to dostaniesz tyle", dolar, "$")
 else:
     print("ile dolarów chcesz zamienić na złoówki?")
     dolar = float(input())
-    zloty = float(dolar*3.8772)
+    zloty = c.convert('USD', 'PLN', dolar)
     print("za to dostaniesz tyle", zloty, "zł")
