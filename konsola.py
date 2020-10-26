7575fb1c91b9f1fc2e7738f720c499c0d7b68798
weronikachr <weronika.chruszcz.2000@gmail.com>
2020-10-26 23:18:26 +0100

    commentsForPresentations

--- konsola.py
+++ konsola.py
@@ -1,16 +1,16 @@
 #1$ = 3,8772zł
 from forex_python.converter import CurrencyRates
-c = CurrencyRates()
+c = CurrencyRates() #przypisanie do zmiennej c funkcji biblioteki
 
 print("Którą podajesz walutę? Jeśli zł napisz zl jesli dolar napisz $")
 waluta = input()
 if waluta == "zl":
-    print("ile złoówek chcesz zamienić na dolary?")
+    print("ile złotówek chcesz zamienić na dolary?")
     zloty = float(input())
-    dolar = c.convert('PLN', 'USD', zloty)
+    dolar = c.convert('PLN', 'USD', zloty) #użycie funkcji biblioteki, która służy do zamiany walut w obecnym kursie
     print("za to dostaniesz tyle", dolar, "$")
 else:
-    print("ile dolarów chcesz zamienić na złoówki?")
+    print("ile dolarów chcesz zamienić na złotówki?")
     dolar = float(input())
     zloty = c.convert('USD', 'PLN', dolar)
     print("za to dostaniesz tyle", zloty, "zł")
